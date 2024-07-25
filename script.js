eeaee1528f72b962373763344ba0525be58afa1c
abinow <abinow.mail@gmail.com>
2024-07-25 11:14:45 +0530

    delete unwanted codes

--- script.js
+++ script.js
@@ -5,17 +5,14 @@ let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
-  slider.classList.add('active');
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
 slider.addEventListener('mouseleave', () => {
   isDown = false;
-  slider.classList.remove('active');
 });
 slider.addEventListener('mouseup', () => {
   isDown = false;
-  slider.classList.remove('active');
 });
 slider.addEventListener('mousemove', (e) => {
   if(!isDown) return;
